a8e965b57a3c17a74f849938031cdbb24969b9cd
Wellington Antonio Zaneze <wellingtonantonio_zaneze@live.com>
2018-02-09 11:02:20 -0200

    Async INotifyPropertyChanged implemented

---
+++ BindingAsync/Controllers/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingAsync.Controllers
+{
+    class Manager
+    {
+        internal Task<string> AlgoAsync()
+        {
+            return Task.Run(() => { return "Hellor World"; });
+        }
+    }
+}
--- BindingAsync/MainWindow.xaml.cs
+++ BindingAsync/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BindingAsync.Controllers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,45 @@ namespace BindingAsync
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
+
+        Manager manager = new Manager();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string propriedade;
+
+        public string Propriedade
+        {
+            get
+            {
+                return this.propriedade;
+            }
+            set
+            {
+                if (this.propriedade != value)
+                {
+                    this.propriedade = value;
+                    if (this.PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Propriedade"));
+                    }
+                }
+            }
+        }
+
+        async void FazAlgo()
+        {
+            Propriedade = await manager.AlgoAsync();
+        }
+
+        
+
     }
 }
